5aabd1dacf53e571b6d65784f9998aaca7ed36ff
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-10-29 16:46:30 +0530

    done the sign part

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@
 import express from "express";
 import { config } from "dotenv";
 import { connectDb } from "../src/lib/mongodb.js";
-config();
-const app = express();
+import cookieParser from "cookie-parser";
+import signroute from "./routes/signin.route.js";
 
 // setup the applicatin run
+config();
+const app = express();
 const port = process.env.PORT;
-await connectDb();
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/v1/signin", signroute);
+
 app.listen(port);
---
+++ src/routes/signin.route.js
@@ -0,0 +1,49 @@
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ */
+import { Router } from "express";
+import { connectDb } from "../lib/mongodb.js";
+import User from "../models/User.js";
+import jsonwebtoken from "jsonwebtoken";
+const router = Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const { email, photoURL, fullName, uid } = req.body;
+
+    //validating data
+    if (!email || !photoURL || !fullName || !uid) {
+      return res.json({ error: "Fields must be required" }).status(400);
+    }
+
+    //saving inforamtion
+    await connectDb();
+    const doc = new User({
+      email,
+      photoURL,
+      fullName,
+      uid,
+    });
+    await doc.save();
+
+    // if failed to create user
+    if (!doc) {
+      return res.json({ error: "Failed to create user" }).status(500);
+    }
+    const token = jsonwebtoken.sign({ _id: doc?._id }, process.env.JWT_SECRET);
+
+    res.cookie("whatsapp", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res
+      .json({ success: true, message: "User created successfully!" })
+      .status(201);
+  } catch (error) {
+    console.log(error);
+    return res.json({ error: error.message }).status(500);
+  }
+});
+
+export default router;
